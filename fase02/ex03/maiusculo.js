07a8dbb361cf3915795440c2cbb80cc1118ce15c
eujoseandre <45535997+eujoseandre@users.noreply.github.com>
2020-01-27 19:00:39 -0300

    Maiúscula

--- fase02/ex03/maiusculo.js
+++ fase02/ex03/maiusculo.js
@@ -1,4 +1,4 @@
-function maiuscula(palavra) {
+function maiusculo (palavra) {
     const beggining = palavra.substr(0, 3).toUpperCase()
     const end = palavra.substr(3);
     return beggining + end;
