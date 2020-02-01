f5313526d34334811193d986b2fb8fe87e31f5fb
Jose Andre <45535997+eujoseandre@users.noreply.github.com>
2020-01-31 21:58:32 -0300

    vai dar certo

---
+++ fase04/ex03/por42.js
@@ -0,0 +1,18 @@
+function por42(num1, num2){
+    
+    var cont = 0;
+    
+    while(num1 <= num2){
+        
+    if(num1%42 === 0){
+        cont++;
+        if(cont === 2){
+            return num1;
+        }
+    }
+    num1++;
+  }
+  if(cont === 0){
+      return "Não encontrado"
+  }
+}
