d280dcd4e808be1cbc096fd9200bb147dd8225a9
Jose Andre <45535997+eujoseandre@users.noreply.github.com>
2020-02-03 18:53:37 -0300

    eh isso

--- fase04/ex00/entreNumeros.js
+++ fase04/ex00/entreNumeros.js
@@ -1,19 +1,6 @@
-function fizzbuzz(num1, num2){
-    
-    while (num1 <= num2){
-        
-    if(num1%3 === 0 && num1%5 ===0){
-        console.log(num1 + " FizzBuzz");
+function entreNumeros(min, max){
+    while(min <= max){
+        console.log(min)
+        min++;
     }
-    else if(num1%5 === 0){
-        console.log(num1 + " Buzz")
-    }
-    else if(num1%3 === 0){
-        console.log(num1 + " Fizz")
-    }
-    else {
-        console.log(num1);
-    }
-    num1++;
-}
 }
