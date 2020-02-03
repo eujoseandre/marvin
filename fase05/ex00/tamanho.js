337eaa4f232d3da98a91beef59365da8d579a9aa
Jose Andre <45535997+eujoseandre@users.noreply.github.com>
2020-02-03 19:18:31 -0300

    eh isso

--- fase05/ex00/tamanho.js
+++ fase05/ex00/tamanho.js
@@ -1,10 +1,3 @@
-function penultimo(array){
-    let cont = 0;
-    let penult = array.length - 2;
-    while(cont <= array.length){
-        if(cont === penult){
-            return array[cont]
-        }
-        cont++;
-    }
+function tamanho(array) {
+    return array.length;
 }
