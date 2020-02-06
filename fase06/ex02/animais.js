fe46ef69407e8385d9355918982a840f2fc78b5c
Jose Andre <45535997+eujoseandre@users.noreply.github.com>
2020-02-05 21:50:27 -0300

    eh isso

---
+++ fase06/ex02/animais.js
@@ -0,0 +1,12 @@
+function animais(array){
+    
+    let num = 5;
+    let i = 0;
+    while (i < array.length){
+    delete array[i].altura;
+    array[i].idade = num;
+    num++;
+    i++;
+}
+    return array
+}
