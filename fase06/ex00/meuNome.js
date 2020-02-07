065d85b11a6702be0a68a791ab07d5ead2a2dd44
Jose Andre <45535997+eujoseandre@users.noreply.github.com>
2020-02-07 20:57:28 -0300

    EH ISSO

--- fase06/ex00/meuNome.js
+++ fase06/ex00/meuNome.js
@@ -1,5 +1,5 @@
 function meuNome(objeto){
     var inicio = objeto.nome.substring(0,1).toUpperCase();
-    var fim = objeto.nome.substring(1);
+    var fim = objeto.nome.substring(1).toLowerCase();
     return inicio + fim;
 }
