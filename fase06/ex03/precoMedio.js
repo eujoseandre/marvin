6bd512281975f09b04ea1bd41513985c2ebd232c
Jose Andre <45535997+eujoseandre@users.noreply.github.com>
2020-02-07 20:09:11 -0300

    eh isso

---
+++ fase06/ex03/precoMedio.js
@@ -0,0 +1,27 @@
+function precoMedio(){
+    
+    let array = arguments;
+    let i = 0;
+    let j = 0;
+    let soma = 0;
+    let arrayValores = [];
+    let valor = 0;
+    let numeroMaior = 0;
+    
+    while (i < arguments.length){
+        soma += array[i].preco;
+        valor = array[i].preco;
+        var total = arrayValores.push(valor)
+        if (arrayValores[i] > numeroMaior){
+        numeroMaior = arrayValores[i];
+        }
+        i++;
+   }
+   while (j < arguments.length){
+        if (array[j].preco == numeroMaior){
+           console.log('O produto mais caro se chama ' + array[j].nome)
+       }
+       j++;
+   }
+    return soma/arguments.length
+}
